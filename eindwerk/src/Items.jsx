685b2e15f600f9d0014df4c3e2f27a316c8da92a
Teun van der Palen <teun.vd.palen@gmail.com>
2022-04-25 13:11:21 +0200

    data gemap

--- eindwerk/src/Items.jsx
+++ eindwerk/src/Items.jsx
@@ -6,24 +6,17 @@ import Item from './Item';
 function Items() {
 	const categorieen = data.categorieen
 
-	const snacks = categorieen[0]
-	const broodjes = categorieen[1]
-	const frieten = categorieen[2]
-
-	const click = () => {
-		console.log(categorieen)
-		console.log(snacks)
-		console.log(broodjes)
-		console.log(frieten)
-		console.log(snacks.producten)
-	}
-
 	return (
 		<div>
-			<button onClick={click}>test</button>
-			<h1 className='text-2xl'>{snacks.naam}</h1>
 			<div>
-				{snacks.producten.map((item, itemIndex) => <Item key={itemIndex} naam={item.naam} prijs={item.prijs} saus={item.sauskeuze} index={itemIndex} />)}
+				{categorieen.map((categorie, itemIndex) => 
+				<div key={itemIndex}>
+					<h1 className='text-2xl'>{categorie.naam}</h1>
+					<div>
+						{categorie.producten.map((item, itemIndex) => <Item key={itemIndex} naam={item.naam} prijs={item.prijs} saus={item.sauskeuze} index={itemIndex} />)}
+					</div>
+				</div>
+				)}
 			</div>
 		</div>
 	)
